cc8f36155b1676e902932215f4bae609c8947156
autotrof <kurniawan@it.student.pens.ac.id>
2017-06-01 23:35:46 +0700

    ANJAY

---
+++ database/migrations/2017_03_19_212724_create_departemen_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartemenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departemen', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('kode');
+            $table->string('nama');
+            $table->string('kadep');
+            $table->string('email_kadep');
+            $table->text('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('departemen');
+    }
+}
